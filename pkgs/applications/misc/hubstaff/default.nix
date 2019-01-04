8d8f1aa372790d95867d6db94d4754ce6fa01ce2
Sergei Khoma <srghma@gmail.com>
2019-01-04 13:20:57 +0200

    hubstaff: fix _S_create_c_locale name not valid

--- pkgs/applications/misc/hubstaff/default.nix
+++ pkgs/applications/misc/hubstaff/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, unzip, makeWrapper, libX11, zlib, libSM, libICE
 , libXext , freetype, libXrender, fontconfig, libXft, libXinerama
 , libXfixes, libXScrnSaver, libnotify, glib , gtk3, libappindicator-gtk3
-, curl }:
+, curl, fetchFromGitHub }:
 
 let
 
@@ -13,6 +13,20 @@ let
     [ libX11 zlib libSM libICE libXext freetype libXrender fontconfig libXft
       libXinerama stdenv.cc.cc.lib libnotify glib gtk3 libappindicator-gtk3
       curl libXfixes libXScrnSaver ];
+
+  LOCALE_ARCHIVE_2_26 =
+    let
+      # A random Nixpkgs revision *before* the default glibc
+      # was switched to version 2.27.x.
+      oldpkgsSrc = fetchFromGitHub {
+        owner = "nixos";
+        repo = "nixpkgs";
+        rev = "0252e6ca31c98182e841df494e6c9c4fb022c676";
+        sha256 = "1sr5a11sb26rgs1hmlwv5bxynw2pl5w4h5ic0qv3p2ppcpmxwykz";
+      };
+
+      oldpkgs = import oldpkgsSrc {};
+    in "${oldpkgs.glibc_2_26}/lib/locale/locale-archive";
 
 in
 
@@ -46,7 +60,7 @@ stdenv.mkDerivation {
 
     for f in "$opt/x86_64/"*.bin.x86_64 ; do
       patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) $f
-      wrapProgram $f --prefix LD_LIBRARY_PATH : ${rpath}
+      wrapProgram $f --prefix LD_LIBRARY_PATH : ${rpath} --set LOCALE_ARCHIVE_2_26 ${LOCALE_ARCHIVE_2_26}
     done
 
     ln -s $opt/x86_64/HubstaffClient.bin.x86_64 $out/bin/HubstaffClient
